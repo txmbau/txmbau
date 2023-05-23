c0f6e26af0b7dab05fcda70dc71c86e5314aea7e
uygnm <uygnm@student.kit.edu>
2023-05-23 14:27:28 +0200

    initial commit

---
+++ Pythontutorial.py
@@ -0,0 +1,36 @@
+#print("Hello, World!")
+#print("""WhY
+#aRe
+#YoU nOt PrINting???""")
+
+#print("I am Iron Man \n" + "No, I am Tony Stark")
+
+#print("HIIII\n" * 10)
+
+#Let's build a barista robot
+
+#print("Hello, welcome to the coffeshop")
+
+#a = input("What is your name?\n")
+
+#print("Hello " + a)
+
+#menu = "dish 1\ndish 2\ndish 3"
+
+#order = input(name + "what would you like to eat from this menu?\n" + menu + "\n")
+
+#print(order + "!!")
+
+#name = "Tim"
+#age = 21
+
+#print(age)
+#print(type(name))
+#print(type(age))
+#print(age ** 2)
+
+name = input("Name?")
+if name == "Ben" :
+    print("Get outta here homie!!")
+else:
+    print("Hello " + name)
